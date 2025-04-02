def463aaa06044c8b73f8dfcae623f009066c0b4
Doxy Gw1n <m.v.orekhov@gmail.com>
2025-04-02 11:05:28 +0200

    Ver. 1.4.1 - unstable

--- 1.4.1/1.4.1.ino
+++ 1.4.1/1.4.1.ino
@@ -1,66 +1,92 @@
+/* Includes */
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
 
-const uint8_t LED1 = 1<<PD5;
-const uint8_t LED2 = 1<<PD6;
-const uint8_t LED12 = 1<<PB4;
-const uint8_t LED13 = 1<<PB5;
-const uint8_t SWITCH1 = 1<<PD3;
+/* Global Variables */
+const uint8_t LED1 = 1 << PD5;
+const uint8_t LED2 = 1 << PD6;
+const uint8_t LED13 = 1<<PB5; //built in LED
+const uint8_t LED4 = 1<<PD4;
+const uint8_t LED5 = 1<<PD7;
+const uint8_t LED6 = 1<<PB0;
+const uint8_t LED7 = 1<<PB1;
 
+
+const uint8_t SWITCH1 = 1<<PD3;
+const uint8_t SWITCH2 = 1<<PD2;
+
+
+/* Function Declarations */
 ISR(INT1_vect);
 void DoSomethingImportant();
 
-int main()
+int main(void)
 {
-  EIMSK |= (1<<INT1);
-  EICRA |= (1<<ISC11);
+    /* Setup */
+    EIMSK |= (1 << INT1);          // Enable ext. INT1
+    EICRA = (1 << ISC11);          // External interrupt on falling edge
 
-  sei();
+    sei();                         // enable interrupts
 
-  DDRB |= (LED12 | LED13);
-  PORTD |= SWITCH1;
-  DDRD |= (LED1 | LED2);
+    DDRB |= LED13;                 // set PB5 to output
+    PORTD |= (SWITCH1 | SWITCH2);              // set pull-up at PD3 and PD2
+    DDRD |= (LED1 | LED2);         // set pins for LED1, LED2 to output pins
 
-  PORTD |= LED1;
+    /* Event Loop */
+    PORTD |= LED1;
+    while (1)
+    {
+        DoSomethingImportant();
+    }
 
-  while(1)
-  {
-    DoSomethingImportant();
-  }
-  return 0;
+    return 0;
 }
 
+/* Interrupt Service Routine (ISR) */
 ISR(INT1_vect)
-  {
-    _delay_ms(5);
-    if((PIND & SWITCH1) == 0)
+{
+    _delay_ms(5); // wait
+    if ((PIND & SWITCH1) == 0) // still pressed?
     {
+        PORTB |= LED13;          // turn on LED13
+        PORTD |= (LED1 | LED2);  // turn on LED1 and LED2
+        _delay_ms(3000);         // wait 3 sec
+
+        PORTB &= ~LED13;         // LED13 off
+        PORTD &= ~(LED1);        // LED1 off
+    }
+    else if((PIND & SWITCH2) == 0)
+    {
+      PORTB |= LED13;          // turn on LED13
       PORTD &= ~LED1;
       PORTD &= ~LED2;
       for (int i = 0; i < 2; i++)
       {
-        PORTD ^= LED1;
+        PORTD ^= LED4;
         _delay_ms(1000);
-        PORTD ^= LED1;
-        PORTD ^= LED2;
+        PORTD ^= LED4;
+        PORTD ^= LED5;
         _delay_ms(1000);
-        PORTD ^= LED2;
-        PORTB ^= LED12;
+        PORTD ^= LED5;
+        PORTB ^= LED6;
         _delay_ms(1000);
-        PORTB ^= LED12;
-        PORTB ^= LED13;
+        PORTB ^= LED6;
+        PORTB ^= LED7;
         _delay_ms(1000);
-        PORTB ^= LED13;
+        PORTB ^= LED7;
       }
-      PORTD |= LED1;
+      PORTB &= ~LED13;         // LED13 off
     }
-  }
+}
+
+
+    
 
 void DoSomethingImportant()
 {
-  PORTD ^= (LED1 | LED2);
-  _delay_ms(1000);
-  return;
+    PORTD ^= (LED1 | LED2);      // toggle LEDs
+    _delay_ms(1000);
+    return;
 }
