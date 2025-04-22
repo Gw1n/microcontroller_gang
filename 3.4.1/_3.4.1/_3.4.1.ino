f6a6d86bc18c8de0fd079692bd2cdbc48a25b1c3
Kuzey <kuzeyergin09@gmail.com>
2025-04-22 18:44:53 +0200

    modul 3 UNTESTED

--- 3.4.1/_3.4.1/_3.4.1.ino
+++ 3.4.1/_3.4.1/_3.4.1.ino
@@ -4,21 +4,19 @@
 #include <avr/interrupt.h>
 int main(void)
 {
-  uint8_t fade_done = 0; // track if fade has already run
   const uint8_t SWITCH1 = (1 << PD3);
   const uint8_t SWITCH2 = (1 << PD2); // bit masks for the switches
   PORTD |= (SWITCH1 | SWITCH2); // activate pull-up resistors
   TCCR0B = (1<<CS02) | (1<<CS00); // 1024 prescaling for Timer0 for a 61Hz output (it's as close to 50Hz as we can get)
   TCCR1B = (1<<CS12) | (1<<CS10) | (1<<WGM12); // 1024 prescaling for Timer1 and WGM12 set for fast PWM
-  DDRD = (1<<PD5) | (1<<PD6) | (1<<PB1) | (1<<PB2); //Pin OC0A and OC0B as OUTPUT
+  DDRD = (1<<PD5) | (1<<PD6); //Pin OC0A and OC0B as OUTPUT
   DDRB = (1<<PB1) | (1<<PB2); // Pin OC1A and OC1B as OUTPUT
   PORTD &= ~((1<<PD5) | (1<<PD6)); //Initialise pin outputs to 0
   OCR0B = 0;
   OCR0A = 0;
   OCR1B = 0;
   OCR1A = 0;//Initialise PWM values to 0
-  uint8_t i_1;
-  uint8_t i_2;
+  uint8_t i;
   while(1) {
     TCCR0A &= ~((1<<COM0A1) | (1<<COM0B1)); // Disconnect PWM from pins because LEDs won't fully turn off othwerwise
     TCCR1A &= ~((1<<COM1A1) | (1<<COM1B1));
@@ -30,12 +28,12 @@ int main(void)
       // Clear OC0A on compare match when up-counting.
       // Clear OC0B on compare match when up-counting.
       // Set on compare match when down 
-      i_1=0;
-      while (i_1 < 255) {
-        OCR0B = i_1;
-        OCR0A = 255-i_1;
+      i=0;
+      while (i < 255) {
+        OCR0B = i;
+        OCR0A = 255-i;
         _delay_ms(10);
-        i_1++;
+        i++;
         }
      }
      if((PIND & SWITCH2) == 0) {  
@@ -44,12 +42,12 @@ int main(void)
        // Clear OC1A on compare match when up-counting
        // Clear OC1B on compare match when up-counting
        // Set on compare match when down
-       i_2 = 0;
-       while (i_2 < 255) {
-        OCR1B = 255-i_2;
-        OCR1A = i_2;
+       i = 0;
+       while (i < 255) {
+        OCR1B = 255-i;
+        OCR1A = i;
         _delay_ms(10);
-        i_2++;
+        i++;
        }
      }
   }
---
+++ 4.3.1/_4.3.1/_4.3.1.ino
@@ -0,0 +1,89 @@
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+void InitUSART(uint32_t baud_rate);
+uint8_t ReceiveByte(void);
+void controlMotor(uint8_t motorNum, int8_t speed);
+void InitTimer1();
+void InitMotors();
+
+void InitUSART(uint32_t baud_rate) {
+  uint16_t ubrr_val = (F_CPU - 8 * baud_rate)/(16 * baud_rate);
+  UBRR0 = ubrr_val;
+  UCSR0B = (1 << RXEN0)|(1 << TXEN0); // enable transmitter and receiver
+  UCSR0C = (1 << UCSZ01)|(1 << UCSZ00); // set data bits to 8 (stop bit is 1 by default)
+}
+
+uint8_t ReceiveByte(void) {
+  do{} while (!(UCSR0A & (1 << RXC0))); // wait until the receive buffer flag is set
+  return UDR0;
+}
+
+void InitTimer1(){
+// Clear OC1A and OC1B on Compare Match / Set OC1A and OC1B at Bottom;
+// Wave Form Generator: Fast PWM 14, TOP = ICR1
+TCCR1A |= (1<<COM1A1) | (1<<COM1B1) | (1<<WGM11);
+TCCR1B |= (1<<WGM13) | (1<<WGM12) | (1<<CS11); // prescaler = 8
+ICR1 = 15999;
+// PWM frequency = (16000000 Hz)/8/(TOP+1) = (1000 Hz)/8 = 125 Hz
+// The value ICR1 together with OCR1A and OCR1B determines the duty cycles
+// observed at pin OC1A (PB1, Pin15) and OC1B (PB2, Pin16), respectively.
+}
+
+void InitMotors() {
+    // Direction control pins as output: PB0 and PB3
+    // PWM output pins as output: PB1 (OC1A connected to ENA), PB2 (OC1B connected to ENB)
+    DDRB |= (1 << PB0) | (1 << PB3) | (1 << PB1) | (1 << PB2);
+}
+
+void controlMotor(uint8_t motorNum, int8_t speed) {
+    uint8_t absSpeed = abs(speed);
+    if (motorNum == 1) {
+        if (speed >= 0) { 
+            PORTB |= (1 << PB0);
+        } else {
+            PORTB &= ~(1 << PB0);
+        }
+        OCR0A = absSpeed; // PWM for Motor 1
+    } else if (motorNum == 2) {
+        if (speed >= 0) {
+            PORTB |= (1 << PB3);
+        } else {
+            PORTB &= ~(1 << PB3);
+        }
+        OCR0B = absSpeed; // PWM for Motor 2
+    }
+}
+
+int main(void) {  
+  InitUSART(9600);
+  InitTimer1();
+  InitMotors();
+  while (1) {
+        char cmd = ReceiveByte();
+        switch (cmd) {
+            case 'w': // forward
+                controlMotor(1, 200);
+                controlMotor(2, 200);
+                break;
+            case 's': // backward
+                controlMotor(1, -200);
+                controlMotor(2, -200);
+                break;
+            case 'a': // turn left
+                controlMotor(1, 150);
+                controlMotor(2, 0);
+                break;
+            case 'd': // turn right
+                controlMotor(1, 0);
+                controlMotor(2, 150);
+                break;
+            default:
+                controlMotor(1, 0);
+                controlMotor(2, 0);
+                break;
+        }
+    }
+}
